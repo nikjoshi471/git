829f2a7354d0f815ed267eb3964d30b3d2527005
Nikhil Joshi <nikjoshi471@.github.com>
2020-05-19 21:00:47 -0400

    try

---
+++ app/src/main/java/c/e/myapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package c.e.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
